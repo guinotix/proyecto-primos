4481e8a2301d44a4d390c4b22f0bfdc5b5aaec7f
guinotix <al375927@uji.es>
2020-11-03 21:56:51 +0100

    primer commit

---
+++ primos.py
@@ -0,0 +1,30 @@
+from random import randint
+
+def es_primo(numero: int) -> bool:
+    if numero < 2:
+        return False
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+        else:
+            return True
+
+
+if __name__ == '__main__':
+
+    cantidad = int(input("Cuantos numeros van a la lista?: "))
+    lista_numeros = []
+    while cantidad > 0:
+        un_numero = randint(1, 10)
+        lista_numeros.append(un_numero)
+        cantidad -= 1
+
+    print(lista_numeros)
+
+    for numero in lista_numeros:
+        if es_primo(numero):
+            print(f"{numero} es primo")
+        else:
+            print(f"{numero} no es primo")
+
+    # Este comentario es para hacer un cambio
